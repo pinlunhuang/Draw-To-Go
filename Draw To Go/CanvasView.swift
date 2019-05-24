29d9d76c132a33343e9f68fafa844e7e75db8e00
pinlun <pudchi330@gmail.com>
2019-05-24 17:11:35 +0800

    Add ML Model and basically finish the app

---
+++ Draw To Go/CanvasView.swift
@@ -0,0 +1,72 @@
+//
+//  CanvasView.swift
+//  Draw To Go
+//
+//  Created by 黃品綸 on 2019/5/24.
+//  Copyright © 2019 Pinlun. All rights reserved.
+//
+
+import UIKit
+
+class CanvasView: UIView {
+    
+    var lineColor: UIColor!
+    var lineWidth: CGFloat!
+    var path: UIBezierPath!
+    var touchPoint: CGPoint!
+    var startingPoint: CGPoint!
+    
+    override func layoutSubviews() {
+        self.clipsToBounds = true
+        self.isMultipleTouchEnabled = false
+        
+        lineColor = UIColor.white
+        lineWidth = 6
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        startingPoint = touch?.location(in: self)
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        touchPoint = touch?.location(in: self)
+        
+        path = UIBezierPath()
+        path.move(to: startingPoint)
+        path.addLine(to: touchPoint)
+        startingPoint = touchPoint
+        
+        drawLayer()
+    }
+    
+    func drawLayer() {
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path.cgPath
+        shapeLayer.strokeColor = lineColor.cgColor
+        shapeLayer.lineWidth = lineWidth
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        self.layer.addSublayer(shapeLayer)
+        self.setNeedsDisplay()
+    }
+    
+    func clearCanvas() {
+        path.removeAllPoints()
+        self.layer.sublayers = nil
+        self.setNeedsDisplay()
+    }
+
+}
+
+
+extension UIImage {
+    convenience init (view: UIView) {
+        UIGraphicsBeginImageContext(view.frame.size)
+        view.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        self.init(cgImage: image!.cgImage!)
+    }
+}
--- Draw To Go/ViewController.swift
+++ Draw To Go/ViewController.swift
@@ -7,14 +7,79 @@
 //
 
 import UIKit
+import Vision
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var canvasView: CanvasView!
+    
+    var request = [VNRequest]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupVision()
     }
-
+    
+    func setupVision() {
+        guard let visionModel = try? VNCoreMLModel(for: MNIST().model) else {
+            fatalError("CANNOT load ML Model")
+        }
+        
+        let classficationRequest = VNCoreMLRequest(model: visionModel, completionHandler: self.handleClassification)
+        
+        self.request = [classficationRequest]
+        
+    }
+    
+    func handleClassification(request: VNRequest, error: Error?) {
+        guard let observation = request.results else {
+            print("No Results"); return
+        }
+        
+        let classification = observation.compactMap({$0 as? VNClassificationObservation})
+        .filter({$0.confidence > 0.8})
+        .map({$0.identifier})
+        
+        DispatchQueue.main.async {
+            self.numberLabel.text = classification.first
+        }
+        
+    }
+    
+    @IBAction func clearCanvas(_ sender: Any) {
+        canvasView.clearCanvas()
+        DispatchQueue.main.async {
+            self.numberLabel.text = "0"
+        }
+    }
+    
+    @IBAction func recognize(_ sender: Any) {
+        let image = UIImage(view: canvasView)
+        let scaleImg = scaleImage(image: image, toSize: CGSize(width: 28, height: 28))
+        
+        let imageRequestHandler = VNImageRequestHandler(cgImage: scaleImg.cgImage!, options: [:])
+        
+        do {
+            try imageRequestHandler.perform(self.request)
+        } catch {
+            print(error)
+        }
+        
+    }
+    
+    func scaleImage(image: UIImage, toSize size: CGSize) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(size, false, 1.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        let newImg = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImg!
+    }
+    
 
 }
 
